cae631695788f5eb037a03a16f775b307d8503bc
Juan Carlos <jc.mateos@ibermatica.com>
2022-10-28 09:33:25 +0200

    Changed application.yml to properties.

--- ProductService/src/test/java/com/inditex/product/service/impl/ProductServiceTest.java
+++ ProductService/src/test/java/com/inditex/product/service/impl/ProductServiceTest.java
@@ -51,7 +51,7 @@ class ProductServiceTest {
       "2020-06-15T10:00:00,30.50", // Test 4: petición a las 10:00 del día 15 del producto 35455 para la brand 1 (ZARA)
       "2020-06-16T21:00:00,38.95", // Test 5: petición a las 21:00 del día 16 del producto 35455 para la brand 1 (ZARA)
   })
-  void findProductsToApplyTest1(final String date, final String prize) throws ProductServiceException {
+  void given_RequestParams_when_FindProductToApply_then_ObtainProductResponse(final String date, final String prize) throws ProductServiceException {
     // Given:
     Integer brandId = 1;
     Long productId = 35455L;
